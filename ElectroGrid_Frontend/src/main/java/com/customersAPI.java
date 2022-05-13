3a4005e3b07c0105837e2fd7c8f21bab8af2e8e4
hiruniSam <88319719+hiruniSam@users.noreply.github.com>
2022-05-13 23:08:53 +0530

    Modification of do put method

--- ElectroGrid_Frontend/src/main/java/com/customersAPI.java
+++ ElectroGrid_Frontend/src/main/java/com/customersAPI.java
@@ -1,6 +1,10 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -57,6 +61,19 @@ public class customersAPI extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request); 
+		 String output = custObj.updateCustomer(paras.get("hidItemIDSave").toString(), 
+				 							paras.get("NIC").toString(), 
+				 							paras.get("CustomerFirstName").toString(), 
+				 							paras.get("CustomerLastName").toString(), 
+				 							paras.get("HomeNo").toString(),
+				 							paras.get("Street").toString(), 
+				 							paras.get("HomeCity").toString(), 
+				 							paras.get("CustomerPhone").toString(), 
+				 							paras.get("AccountNo").toString(), 
+				 							paras.get("Password").toString()); 
+		response.getWriter().write(output); 
 	}
 
 	/**
@@ -65,5 +82,30 @@ public class customersAPI extends HttpServlet {
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+	
+	
+	
+	// Convert request parameters to a Map
+		private static Map getParasMap(HttpServletRequest request) 
+		{ 
+		 Map<String, String> map = new HashMap<String, String>(); 
+		try
+		 { 
+		 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+		 String queryString = scanner.hasNext() ? 
+		 scanner.useDelimiter("\\A").next() : ""; 
+		 scanner.close(); 
+		 String[] params = queryString.split("&"); 
+		 for (String param : params) 
+		 {
+			 String[] p = param.split("=");
+			 map.put(p[0], p[1]); 
+			 } 
+			 } 
+			catch (Exception e) 
+			 { 
+			 } 
+			return map; 
+			}
 
 }
